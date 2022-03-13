7bffc3a8aa3ae7f94de762fb9cb6cfb93477f0af
Zorinalexew <zorin2806@gmail.com>
2022-03-13 23:43:43 +0300

    2.py 1st commit

---
+++ Zorin/2.py
@@ -0,0 +1,9 @@
+#Написать функцию подключения к базе данных
+import mysql.connector as conn
+
+mydb = conn.connect(host = '127.0.0.1', database='users',user="root",password="")
+login = input()
+c = mydb.cursor()
+c.execute(f"SELECT * FROM login WHERE login = '{login}'")
+user = c.fetchall()
+print(user)
