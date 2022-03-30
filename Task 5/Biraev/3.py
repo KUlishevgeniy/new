266b1d79a8ce22f568dd9dee5f266d4e469867c4
Nurazim <mrpaleluna003006002@gmail.com>
2022-03-30 08:42:46 +0300

    5

---
+++ Task 5/Biraev/3.py
@@ -0,0 +1,27 @@
+from tkinter import *
+root = Tk()
+root.geometry("300x300")
+root.title("Форма 1")
+surname = StringVar()
+name_label = Label(text="Введите имя")
+surname_label = Label(text="Введите фамилию")
+name_label.grid(row=1, column=0, sticky="w")
+surname_label.grid(row=2, column=0, sticky="w")
+
+def auth():
+    sa=name.get() + " " + surname.get()
+    window1 = Tk()
+    window1.title("Окно 2")
+    window1.geometry('800x600')
+    lbl1 = Label(window1, text=sa)
+    lbl1.grid(column=0, row=0)
+    window1.mainloop()
+
+
+surname_entry = Entry(textvariable=surname)
+name = StringVar()
+name_entry = Entry(textvariable=name)
+surname_entry.grid(row=2, column=1, padx=5, pady=5)
+name_entry.grid(row=1, column=1, padx=5, pady=5)
+message_button = Button(text="Авторизация", command= auth)
+message_button.grid(row=3, column=1, pady=5, padx=5, sticky="e")
