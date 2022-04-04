c98f78f32de30ad7839a80d5728ebc7eba3d1487
Елизавета Баранова <baranovaelizaveta017@gmail.com>
2022-04-04 20:41:17 +0300

    commit 5th

--- Baranova/Task5.py
+++ Baranova/Task5.py
@@ -1,7 +1,5 @@
-import mysql.connector as conn
 from tkinter import *
-from tkinter import messagebox
-from UserClass import User
+from UserClass2 import User
 
 window = Tk()
 window.title("Окно Авторизации")
@@ -15,23 +13,11 @@ base_padding = {'padx': 10, 'pady': 8}
 header_padding = {'padx': 10, 'pady': 12}
 
 
-def Entrydef():
-    db = conn.connect(host='127.0.0.1', database='users', user="root", password="")
+def newEntrydef():
+    global user
     login = loginEntry.get()
     password = passwordEntry.get()
-    curcor = db.cursor()
-    curcor.execute(f"SELECT * FROM user_id WHERE login = '{login}'AND password = '{password}'")
-    user = curcor.fetchall()
-
-    if len(user) != 0:
-        window_2 = Tk()
-        window_2.title("Вход")
-        window_2.geometry("200x200")
-        lbll = Label(window_2, text=user[0])
-        lbll.grid(column=0, row=0)
-        window_2.mainloop()
-    else:
-        messagebox.showerror('Ошибка', 'Неправильное имя пользователя или пароль')
+    user = User(login, password)
 
 
 login = Label(window, text="Введите логин:", font=font_header, justify=CENTER, **header_padding)
@@ -44,11 +30,9 @@ loginEntry.grid(column=1, row=0)
 passwordEntry = Entry(window, width=15)
 passwordEntry.grid(column=1, row=1)
 
-send_but = Button(window, text="Войти", command=Entrydef)
+send_but = Button(window, text="Войти", command=newEntrydef)
 send_but.grid(column=1, row=2)
 
 window.mainloop()
 
-user1 = User("maria")
-print(user1.userid)
-print(dir(user1))
+print(user.userid)
