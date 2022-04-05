f536d050f63356f19b42b44c940745587616024a
Fallenhacker29 <56514528+Fallenhacker29@users.noreply.github.com>
2022-04-05 10:19:48 +0300

    Final push parcer

--- Task 2/Monastyrskiy/parce.py
+++ Task 2/Monastyrskiy/parce.py
@@ -8,9 +8,18 @@ import re
 from tqdm import tqdm
 import sys
 import pandas as pd
+import urllib3
+import os.path as path
+from copy import copy
+
+
+urllib3.disable_warnings()
 #import colorama as color 
-blacklisted_url = ["https://store77.net/chasy_apple_watch_nike_se"]
+blacklisted_url = ["https://store77.net/chasy_apple_watch_nike_se","https://store77.net/service_center","https://store77.net/apple_imac_pro_27","https://store77.net/chasy_apple_watch_nike_se",
+                   "https://store77.net/programmy_i_uslugi/sozdanie_uchetnoy_zapisi_apple_id_ili_google","https://store77.net/naushniki_samsung"]
 goods = []
+links = []
+
 sys.setrecursionlimit(100)
 class NotOKResponseCode(Exception):
     def __init__(self,message):
@@ -23,6 +32,92 @@ logging.basicConfig(format='%(asctime)s %(message)s')
 
 #color.init(autoreset=True)
 
+
+
+def trans(text):
+    matrix = {
+"а":"a"
+,"б":"b"
+,"в":"v"
+,"г":"g"
+,"д":"d"
+,"е":"e"
+,"ё":"yo"
+,"ж":"zh"
+,"з":"z"
+,"и":"i"
+,"й":"y"
+,"к":"k"
+,"л":"l"
+,"м":"m"
+,"н":"n"
+,"о":"o"
+,"п":"p"
+,"р":"r"
+,"с":"s"
+,"т":"t"
+,"у":"u"
+,"ф":"f"
+,"х":"h"
+,"ц":"ts"
+,"ч":"ch"
+,"ш":"sh"
+,"щ":"shch"
+,"ъ":""
+,"ы":"y"
+,"ь":""
+,"э":"eh"
+,"ю":"yu"
+,"я":"ya"
+," ":"_"
+,"А":"A"
+,"Б":"B"
+,"В":"V"
+,"Г":"G"
+,"Д":"D"
+,"Е":"E"
+,"Ё":"Yo"
+,"Ж":"Zh"
+,"З":"Z"
+,"И":"I"
+,"Й":"Y"
+,"К":"K"
+,"Л":"L"
+,"М":"M"
+,"Н":"N"
+,"О":"O"
+,"П":"P"
+,"Р":"R"
+,"С":"S"
+,"Т":"T"
+,"У":"U"
+,"Ф":"F"
+,"Х":"H"
+,"Ц":"Ts"
+,"Ч":"Ch"
+,"Ш":"Sh"
+,"Щ":"Shch"
+,"Ъ":""
+,"Ы":"Y"
+,"Ь":""
+,"Э":"Eh"
+,"Ю":"Yu"
+,"Я":"Ya"
+     }
+    comp_change = {
+" ":"_",
+"(":"_",
+")":"_",
+".":"_",
+"/":"_",
+"\\":"_",
+"&quot":"_"
+}
+    for char in matrix.keys():
+        text = text.replace(char,matrix[char])
+    for char in comp_change.keys():
+        text = text.replace(char,comp_change[char])
+    return text
 try:
     config = configparser.ConfigParser()
     config.read(CONFIG_FILE)
@@ -38,7 +133,7 @@ except Exception:
 
 def GetDataFromURL(url: str) -> str:
     try:
-        response = r.get(url)
+        response = r.get(url,verify=False)
     except Exception:
         raise NotOKResponseCode("Connection died")
     if response.status_code == 200:
@@ -50,7 +145,7 @@ def ParceStore77Net_Sections(url):
     try:
         resp = GetDataFromURL(url)
     except NotOKResponseCode:
-        print(color.Fore.RED + "Seems like host is down, check manually")
+        print("Seems like host is down, check manually")
     soup = bs(resp,"lxml")
     sections = soup.find_all("ul",class_ = "catalog_menu_sub_third")
     #print(sections)
@@ -91,7 +186,7 @@ def ParceStore77Net_Sections(url):
 
 
 def Insert_to_database(file):
-    df.read_excel(file)
+    df = pd.read_excel(file)
     try:
         mydb=conn.connect(host=host,database=dbname,user=login,password=password)
         c = mydb.cursor()
@@ -99,7 +194,7 @@ def Insert_to_database(file):
         print(e)
     df = df.reset_index()
     for index, row in df.iterrows():
-        c.execute(f"INSERT INTO catalog (art,name,price,brand,category) VALUES ('{row['arts'][1:-2]}','{row['name'][1:-2]}','{str(row['row'][0:-1])}',{row['brand'][1:-2]},{row['cat'][1:-2]})")
+        c.execute(f"INSERT INTO catalog (art,name,price,brand,category) VALUES ('{row['arts'][1:-2]}','{row['name'][1:-2]}','{str(row['price'][0:-1])}',{row['brand'][1:-2]},{row['cat'][1:-2]})")
         mydb.commit()
 
 
@@ -111,9 +206,10 @@ def SaveData():
     price = [elem[2] for elem in goods]
     brand = [elem[3] for elem in goods]
     cat = [elem[4] for elem in goods]
-    if not path.exists("res.xlsx")
+    dlink = [elem[-1] for elem in goods]
+    if not path.exists("res.xlsx"):
         for i in tqdm(range(0,len(arts))):
-            df = pd.DataFrame({"arts":arts,"name":name,"price":price,"brand":brand,"cat":cat})
+            df = pd.DataFrame({"arts":arts,"name":name,"price":price,"brand":brand,"cat":cat,"link":dlink})
             df.to_excel("res.xlsx")
             Insert_to_database("res.xlsx")
     else:
@@ -123,6 +219,7 @@ def SaveData():
 
 def ParceStore77Net_EachSection(link):
     print("New line")
+    l = copy(link)
     try:
         response = GetDataFromURL(link)
     except RecursionError:
@@ -135,7 +232,10 @@ def ParceStore77Net_EachSection(link):
     soup = bs(response,"lxml")
     data = soup.find_all("a",class_="bp_hover_text_but_cart")
     #print(data)
+    print(link)
+    global links
     data = str(data).split(' onclick="dataLayer.push({')
+    link = data[0]
     #print(data[3])
     parced_data = []
     for elem in data:
@@ -153,7 +253,10 @@ def ParceStore77Net_EachSection(link):
             price = property_[3][1]
             brand = property_[4][1]
             cat = property_[5][1]
-            parced_data.append([art,name,price,brand,cat])
+            dlink =l+"/"+trans(name)
+            dlink =dlink[2:-3]
+            print(dlink)
+            parced_data.append([art,name,price,brand,cat,dlink])
         except Exception:
             pass
     global goods
