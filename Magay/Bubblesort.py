c5aa04180df6101b4202fb2c735e44eb82427b42
MGVhanda <99758490+MGVhanda@users.noreply.github.com>
2022-04-11 20:58:28 +0300

    Update Bubblesort.py

--- Magay/Bubblesort.py
+++ Magay/Bubblesort.py
@@ -1,1 +1,12 @@
+def sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
+arr = [84, 34, 25, 12, 22, 21, 91]
+sort(arr)
+print ("Сортированный массив:")
+for i in range(len(arr)):
+    print ("%d" %arr[i])
