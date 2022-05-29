d4d2e6e7d4091e6afe2d1d712894fb79f375c650
Fallenhacker29 <56514528+Fallenhacker29@users.noreply.github.com>
2022-05-29 18:49:02 +0300

    Django correct push

--- Task 2/Monastyrskiy/myweb/myweb/views.py
+++ Task 2/Monastyrskiy/myweb/myweb/views.py
@@ -1,11 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import Http404,HttpResponse
-
-
 def index(request):
-	print()
-	render(request,"\static\index.html")
-
-
-
-# Create your views here.
+	return render(request,'index.html')
